1089d67071d134ddae921daddd594ce01386ccc4
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2023-03-21 04:37:00 +0000

    Add bevy feature flag, TypeUuid implementation for RustGpuBuilderOutput

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,14 @@ pub struct RustGpuBuilderOutput {
     pub entry_points: Vec<String>,
     pub modules: RustGpuBuilderModules,
 }
+
+#[cfg(feature = "bevy")]
+mod bevy {
+    use super::RustGpuBuilderOutput;
+    use bevy::{reflect::TypeUuid, utils::Uuid};
+
+    impl TypeUuid for RustGpuBuilderOutput {
+        const TYPE_UUID: Uuid =
+            Uuid::from_fields(1664188495, 11437, 8530, &[15, 75, 77, 14, 32, 11, 25, 52]);
+    }
+}
