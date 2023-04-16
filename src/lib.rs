2810d6c8b8e7b58ff85d5edc27993b51b2f1ded3
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2023-04-16 22:49:51 +0100

    Document RustGpuBuilderModules / RustGpuBuilderOutput

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,17 @@ use std::collections::BTreeMap;
 
 use serde::{Deserialize, Serialize};
 
+/// Module binary data container.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum RustGpuBuilderModules {
+    /// Contains a single unnamed module.
     Single(Vec<u8>),
+    /// Contains multiple named modules.
     Multi(BTreeMap<String, Vec<u8>>),
 }
 
+/// Compile output from `rust-gpu-builder`,
+/// includes SPIR-V binary modules and entry point metadata.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct RustGpuBuilderOutput {
     pub entry_points: Vec<String>,
@@ -19,6 +24,7 @@ mod bevy {
     use super::RustGpuBuilderOutput;
     use bevy::{reflect::TypeUuid, utils::Uuid};
 
+    /// Implementing TypeUuid allows use as a `bevy` asset
     impl TypeUuid for RustGpuBuilderOutput {
         const TYPE_UUID: Uuid =
             Uuid::from_fields(1664188495, 11437, 8530, &[15, 75, 77, 14, 32, 11, 25, 52]);
